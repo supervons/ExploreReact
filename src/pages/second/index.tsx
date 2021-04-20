1ea4e3007446be564a8cdf79d21bc3e16830f7f5
supervons <supervons@sina.com>
2021-04-20 10:34:56 +0800

    config project path.

--- src/pages/second/index.tsx
+++ src/pages/second/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import service from '../../utils/service';
+import service from 'utils/service';
 import './index.css';
 import { Link } from 'react-router-dom';
 
