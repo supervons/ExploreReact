d577f30c396cf57dda6216550a87850846643db3
supervons <supervons@sina.com>
2022-02-28 17:56:05 +0800

    Abandon unuse host.

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     createProxyMiddleware('/commonProject', {
-      target: 'http://47.93.31.98:8080',
+      target: 'http://xxxx:8080',
       changeOrigin: true
       // ,pathRewrite: {
       //   '/commonProject': ''
